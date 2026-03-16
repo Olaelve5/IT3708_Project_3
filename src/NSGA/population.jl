756586e518c53b6a8c91fdf1f9a1d5cb68657ee8
Pizzaman9988 <supershooter@live.no>
2026-03-16 13:27:08 +0100

    - Population generator done.

--- src/NSGA/population.jl
+++ src/NSGA/population.jl
@@ -1,4 +1,4 @@
-struct Individual
+mutable struct Individual
     genome::Bitvector
     accuracy::Float64
     num_features::Int
@@ -6,14 +6,14 @@ struct Individual
     crowding_distance::Float64
 end
 
-function new_individual(chromosome::Bitvector)::Individual
-    return Individual(chromosome, 0.0, sum(chromosome), 0, 0.0)
+function new_individual(chromosome::Bitvector, accuracy::Float64)::Individual
+    return Individual(chromosome, accuracy, sum(chromosome), 0, 0.0)
 end
 
-function init_pop(pop_size::Int)::Vector{Individual}
+function init_pop(pop_size::Int, n_features::Int, evaluate::Function)::Vector{Individual}
     population = Vector{Individual}(undef, pop_size)
-    genes = generate_population(pop_size, 16)
+    genes = generate_population(pop_size, n_features)
     for i in 1:pop_size
-        population[i] = new_individual(genes[i])
+        population[i] = new_individual(genes[i], evaluate(genes))
     end
 end
