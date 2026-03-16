973a0701a30dd7281213319409b13f2c7f80738b
Pizzaman9988 <supershooter@live.no>
2026-03-16 11:25:37 +0100

    - Fixed function signature

--- src/common.jl
+++ src/common.jl
@@ -1,6 +1,6 @@
 using EvoLP
 
 # Bitvector population generator
-function generate_population(pop_size::Int, chromosome_length::Int)::BitVector
+function generate_population(pop_size::Int, chromosome_length::Int)::Vector{BitVector}
     return binary_vector_pop(pop_size, chromosome_length)
 end
